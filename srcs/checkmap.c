708ed34bcbda485a3464a3d35a64272fac3fe304
Goulven Traimany <gtraiman@paul-f5Ar3s7.clusters.42paris.fr>
2025-02-26 19:05:57 +0100

    1st push, checkmap to impove (111 1)

---
+++ srcs/checkmap.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checkmap.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gtraiman <gtraiman@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 16:23:07 by gtraiman          #+#    #+#             */
+/*   Updated: 2025/02/26 19:04:37 by gtraiman         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "checkmap.h"
+
+int	ft_slen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int checkmap(char **tab)
+{
+    int i = 0;
+    int j = -1;
+    
+    while(tab[0][++j])
+    if(tab[0][j] != '1' && tab[0][j] != ' '&& tab[0][j] != '\t')
+        return(1);
+    while(tab[++i])
+    {
+        j = -1;
+        if(tab[i][0] != '1')
+            return(1);
+        while(tab[i][++j])
+        {
+            if(tab[i][j] != '1')
+                if((ft_slen(tab[i - 1]) < j || ft_slen(tab[i + 1]) < j))
+                    return(1);
+        }
+        if(tab[i][j] && tab[i][j] != '1')
+            return(1);        
+    }
+    j = -1;
+    while(tab[i-1][++j])
+    if(tab[i-1][j] != '1' && tab[0][j] != ' '&& tab[0][j] != '\t')
+        return(1);
+    return(0);
+}
+
+int main(int ac, char **av)
+{
+    if(ac < 2)
+        return(printf("Error"));
+    char *tab[] = 
+    {
+        "1111 1",
+        "100101",
+        "101001",
+        "1100N1",
+        "111111",
+        NULL
+    };
+    printf("%d",checkmap(tab));
+}
+
+
