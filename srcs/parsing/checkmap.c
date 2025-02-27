e435eaed8254d44017a7ef100ec77082923231c6
Goulven Traimany <gtraiman@bess-f4r5s7.clusters.42paris.fr>
2025-02-27 16:02:42 +0100

    checkmap good

--- srcs/parsing/checkmap.c
+++ srcs/parsing/checkmap.c
@@ -6,54 +6,90 @@
 /*   By: gtraiman <gtraiman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 16:23:07 by gtraiman          #+#    #+#             */
-/*   Updated: 2025/02/26 19:04:37 by gtraiman         ###   ########.fr       */
+/*   Updated: 2025/02/27 16:02:13 by gtraiman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checkmap.h"
 
-int	checkmap(char **tab)
+int	ft_slen(char *s)
 {
 	int	i;
-	int	j;
 
 	i = 0;
-	j = 0;
-	while (tab[0][i])
-		if (tab[0][i] != '1' && tab[0][i] != ' ' && tab[0][i] != '\t')
-			return (1);
-		else
-			i++;
-	while (tab[++i])
+    if(!s)
+        return(0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	ft_cins(char *str, char c)
+{
+	while (*str)
 	{
-		if (tab[i][0] != '1')
+		if (*str == c)
 			return (1);
-		j = 0;
-		while (tab[i][++j])
-			if (tab[i][j] != '1' && (ft_strlen(tab[i - 1]) <= (size_t)j || ft_strlen(tab[i + 1]) <= (size_t)j))
-				return (1);
-		if (tab[i][j] && tab[i][j] != '1')
-			return (1);
+		str++;
 	}
-	j = -1;
-	while (tab[i - 1][++j])
-		if (tab[i - 1][j] != '1' && tab[i - 1][j] != ' ' && tab[i - 1][j] != '\t')
-			return (1);
 	return (0);
 }
 
-/*int main(int ac, char **av)
+int checkmap(char **tab)
 {
-    if(ac < 2)
-        return(printf("Error"));
-    char *tab[] =
+    int i;
+    int j;
+    int cpt;
+
+    i = 0;
+    j = 0;
+    cpt = 0;
+    while(tab[i][j])
     {
-        "1111 1",
-        "100101",
-        "101001",
-        "1100N1",
-        "111111",
-        NULL
-    };
-    printf("%d",checkmap(tab));
-}*/
+        if(ft_cins("1 ",tab[i][j]) != 1)
+            return(1);
+        j++;
+    }
+    i++;
+    while(tab[i])
+    {
+        if(tab[i][0] == '0')
+            return(1);
+        j = 1;
+        while(tab[i][j])
+        {
+            if(ft_cins("0NEWS",tab[i][j]) == 1)
+            {
+                if(tab[i][j] != '0')
+                    cpt++;
+                if((ft_slen(tab[i - 1]) < j || ft_slen(tab[i + 1]) < j))
+                    return(1);
+                if(ft_cins("0NEWS1",tab[i - 1][j]) == 0 || ft_cins("0NEWS1",tab[i + 1][j]) == 0)
+                    return(1);
+                if((j != 0 && ft_cins("0NEWS1",tab[i][j - 1]) == 0) || ft_cins("0NEWS1",tab[i][j + 1]) == 0)
+                    return(1);
+            }
+            j++;
+        }
+        i++;
+    }
+    if(cpt != 1)
+        return(1);
+    return(0);
+}
+
+// int main(int ac, char **av)
+// {
+//     if(ac < 2)
+//         return(printf("Error"));    
+//     char *tab[] = 
+//     {
+//         "11111111111111",
+//         "10010000001",
+//         "10100111111",
+//         "1100N1",
+//         "111111",
+//         NULL
+//     };
+//     printf("%d",checkmap(tab));
+// }
