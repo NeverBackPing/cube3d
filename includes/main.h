06b724f106488110665e5edab0b60522d1bbac3d
NeverBackPing <neverbackping@gmail.com>
2025-02-27 01:18:44 +0100

    add struct texture

--- includes/main.h
+++ includes/main.h
@@ -11,6 +11,16 @@
 # include "../dependency/gnl/get_next_line.h"
 # include "../srcs/parsing/checkmap.h"
 
+typedef struct s_texture
+{
+	char	*north_texture;
+	char	*east_texture;
+	char	*south_texture;
+	char	*west_texture;
+	int		ground_color[3];
+	int		roof_color[3];
+} t_texture;
+
 typedef struct s_map
 {
 	char	**map;
@@ -18,8 +28,9 @@ typedef struct s_map
 
 typedef struct s_game
 {
-	int		fd;
-	t_map	map;
+	int			fd;
+	t_map		map;
+	t_texture	texture;
 } t_game;
 
 //parsing/ft_get_map.c
