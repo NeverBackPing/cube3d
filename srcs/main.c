1d6ab0c7a83d1ccc44de44e47a828bca8636ada3
Stevenson Jossaint <neverbackping@gmail.com>
2025-03-03 15:41:51 +0100

    main edit

--- srcs/main.c
+++ srcs/main.c
@@ -14,24 +14,15 @@
 
 int	main(int ac, char **av)
 {
-	// t_game	game;
+	t_game	game;
 
-	// if (ac != 2)
-	// 	handler_argument(ac);
-	// handler_fd(av[1]);
-	(void)ac;
-	(void)av;
-	/*
-		1.	open the fd a first time to retrieve the size of the map, close the fd after.
-			and store it in length which is in the amap structure
-
-		2.	open the fd again to recover the graphic characters
-			if an error is found close the fd and exit with a message
-
-		!! the fd must be stored in the main structure game !!
-	*/
-	// get_set_graph(&game, av[1]);
+	init_struct(&game);
+	if (ac != 2)
+		handler_argument(ac);
+	handler_fd(av[1]);
+	get_set_graph(&game, av[1]);
 	ft_window();
+	free_ressource(&game);
 	// get_map(&game, av[1]);
 	return (0);
 }
