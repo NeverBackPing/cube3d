f83df3709950317940d06c3b1e1efb374a1d03b0
NeverBackPing <neverbackping@gmail.com>
2025-03-03 21:45:02 +0100

    comment function in main

--- srcs/main.c
+++ srcs/main.c
@@ -21,7 +21,7 @@ int	main(int ac, char **av)
 		handler_argument(ac);
 	handler_fd(av[1]);
 	get_set_graph(&game, av[1]);
-	get_lenght_map(&game);
+	//get_lenght_map(&game);
 	//get_map(&game);
 	//ft_window();
 	free_ressource(&game);
