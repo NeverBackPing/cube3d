5df50b47cdfbb9a64e15838861efd6381391546d
Stevenson Jossaint <neverbackping@gmail.com>
2025-03-18 15:41:51 +0100

    fix color roof and floor

--- srcs/window/color.c
+++ srcs/window/color.c
@@ -26,9 +26,9 @@ void	ft_draw_background(t_game *game, char *data, int size_line)
 		while (x < SCREENX)
 		{
 			if (y < SCREENY / 2)
-				img_data[y * (size_line / 4) + x] = game->txt.roof_color;
+				img_data[y * (size_line / 4) + x] =game->txt.ground_color;
 			else
-				img_data[y * (size_line / 4) + x] = game->txt.ground_color;
+				img_data[y * (size_line / 4) + x] = game->txt.roof_color;
 			x++;
 		}
 		y++;
